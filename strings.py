7e3cf3cabd6a5926b10637c60ca25fd2fb8f602b
Hackmaass <122169437+Hackmaass@users.noreply.github.com>
2025-12-24 15:32:15 +0530

    math

---
+++ strings.py
@@ -0,0 +1,20 @@
+#print string to the console
+
+print("Hello, World!")  
+
+print("""
+Hello,
+World!, this string spans multiple lines.
+""")
+
+
+print('Hello, ' + 'World!')  # this string can concatenate 
+
+print('\n')  # this prints a new line character
+print("test that new line above is working")
+print ('\t')  # this prints a tab character
+print ("test that tab above is working")
+print ('\\')  # this prints a backslash character
+print("test that backslash above is working")
+print ('I\'m learning Python!')  # this prints a single quote within a string
+print("line multiple " * 3)  # this prints the string multiple three times
